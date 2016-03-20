03b651abba91530bcc6dbb03d2b39ee4764aa7f6
Hector Reyes <hector.reyes@globant.com>
2016-03-20 11:34:58 -0300

    Template

---
+++ app/app.js
@@ -0,0 +1,98 @@
+(function () {
+  'use strict';
+  angular.module('myApp.filters', []);
+  angular.module('myApp.Services', []);
+  angular.module('myApp.directives', []);
+  angular.module('myApp.controllers', []);
+  angular.module('myApp', [
+    'ngMaterial',
+    'ngMdIcons',
+    'myApp.controllers',
+    'myApp.directives',
+    'myApp.Services',
+    'myApp.filters'
+  ]).config(function ($mdIconProvider, $mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('amber')
+        .accentPalette('orange');
+
+    $mdIconProvider
+        .iconSet('social', 'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg', 24)
+        .iconSet('action', 'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg', 24)
+        .iconSet('navigation', 'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg', 24)
+        .defaultIconSet('bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg', 24);
+  });
+
+  angular.module('myApp.controllers')
+      .controller('controller',
+          ['$scope', '$mdMedia', '$mdSidenav',
+            function ($scope, $mdMedia, $mdSidenav) {
+              $scope.$mdMedia = $mdMedia;
+              $scope.urlIcon = "bower";
+              $scope.cantCard = [
+                {},
+                {},
+                {},
+                {},
+                {},
+                {}
+              ];
+              $scope.showSearch = false;
+
+              $scope.openSiderNav_Left = function () {
+                $mdSidenav('left').toggle();
+              }
+              $scope.Jobs = [
+                {
+                  'title': 'developer',
+                  'company': 'Name of company',
+                  'description': ' description',
+                   'skill': [
+                     { 'name': 'HTML', 'val':80},
+                     { 'name': 'JAVA', 'val':60},
+                     { 'name': 'CSS', 'val':50}
+                   ]
+                },
+                {
+                  'title': 'developer',
+                  'company': 'Name of company',
+                  'description': ' description',
+                  'skill': [
+                    { 'name': 'HTML', 'val':80},
+                    { 'name': 'JAVA', 'val':60},
+                    { 'name': 'CSS', 'val':50}
+                  ]
+                },
+                {
+                  'title': 'developer',
+                  'company': 'Name of company',
+                  'description': ' description',
+                  'skill': [
+                    { 'name': 'HTML', 'val':80},
+                    { 'name': 'JAVA', 'val':60},
+                    { 'name': 'CSS', 'val':50}
+                  ]
+                },
+                {
+                  'title': 'developer',
+                  'company': 'Name of company',
+                  'description': ' description',
+                  'skill': [
+                    { 'name': 'HTML', 'val':80},
+                    { 'name': 'JAVA', 'val':60},
+                    { 'name': 'CSS', 'val':50}
+                  ]
+                },
+                {
+                  'title': 'developer',
+                  'company': 'Name of company',
+                  'description': ' description',
+                  'skill': [
+                    { 'name': 'HTML', 'val':80},
+                    { 'name': 'JAVA', 'val':60},
+                    { 'name': 'CSS', 'val':50}
+                  ]
+                },
+              ];
+            }])
+})();
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var sass = require('gulp-sass'),
+    gulp = require('gulp'),
+    chmod = require('gulp-chmod'),
+    inputFrontend = 'app/scss/*.scss';
+var browserSync = require('browser-sync');
+
+//Compile sass
+gulp.task('sassFrontend', function () {
+  gulp.src(inputFrontend)
+      .pipe(sass({sourceComments: 'map'}))
+      .pipe(chmod(755))
+      .pipe(gulp.dest('app/css'))
+      .pipe(browserSync.reload({stream:true}));
+});
+gulp.task('sass', ['sassFrontend']);
+
+gulp.task('html', function () {
+  gulp.src('app/*.html')
+      .pipe(browserSync.reload({stream:true}));
+});
+gulp.task('js', function () {
+  gulp.src('app/*.js')
+      .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(inputFrontend, ['sass']);
+  gulp.watch("app/*.html", ['html']);
+  gulp.watch("app/*.js", ['js']);
+});
+
+//Liver reload
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: "app"
+    }
+  });
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
